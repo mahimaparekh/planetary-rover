1355b460d49c09c62e5fecff960aed86e4e2724b
mahimaparekh <mahimaparekh98@gmail.com>
2025-12-29 23:50:54 +0530

    remove comments

--- reflex.py
+++ reflex.py
@@ -1,28 +1,33 @@
 import math
+
 class Reflex:
 
     def __init__(self, environment):
         self.env = environment
     
     def set_goal(self, battery, curr_pos):
-        if(battery<=20):
+        if battery <= 0:
+            return 'Dead'
+        elif battery <= 20:
             return self.find_nearest_station(curr_pos)
-        elif(battery>20 and battery<=25):
+        elif battery <= 25:
             station = self.find_nearest_station(curr_pos)
-            if math.dist(curr_pos,station)<=2:
+            if station and math.dist(curr_pos, station) <= 2:
                 return station
             else:
                 return self.env.goal
         else:
             return self.env.goal
         
-    def find_nearest_station(self,curr_pos):
+    def find_nearest_station(self, curr_pos):
         stations = self.env.stations
+        if not stations:
+            return None
         nearest = None
         min_dist = float('inf')
         for s in stations:
-            distance = math.dist(s,curr_pos)
-            if(distance<min_dist):
+            distance = math.dist(s, curr_pos)
+            if distance < min_dist:
                 min_dist = distance
                 nearest = s
         return nearest
