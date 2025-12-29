658c7fdcade6fd8a61bfb49280236a3094700c9e
mahimaparekh <mahimaparekh98@gmail.com>
2025-12-30 00:24:03 +0530

    add code to run rover navigation

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-class main:
-    # create an env object
-    # 
-    # pass the grid size to the env class
-    # 
+import math
+from env import Environment
+from rover import Rover
+
+env = Environment(size=30, stations=30, seed=42)
+rover = Rover(environment=env, battery=100, heuristic='manhattan')
+rover.run()
